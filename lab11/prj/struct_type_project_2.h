034b1be21a4960043f0a47a6c99ab79aa223fd43
Okarpets <karpetsoleksandrschool10@gmail.com>
2024-04-22 14:52:26 +0300

    Have analyzed all tasks held a meeting with team, create architct and struct_type_project_2.h, distributed responsibilities

---
+++ lab11/prj/struct_type_project_2.h
@@ -0,0 +1,24 @@
+#ifndef STRUCT_TYPE_PROJECT_2_H_INCLUDED
+#define STRUCT_TYPE_PROJECT_2_H_INCLUDED
+
+struct Employee {
+    string fillDate;
+    int rollNumber;
+    int ID;
+    string gender;
+    string typeOfWork;
+    string fullName;
+    string dateOfBirthday;
+    string citizenship;
+    string education;
+    string lastWork;
+    string positionOnLastWork;
+    string workExperience;
+    string placeOfresidence;
+    string passportData;
+    string moreInf;
+    string dateOfDismissal;
+    string reasonForDisissal;
+};
+
+#endif // STRUCT_TYPE_PROJECT_2_H_INCLUDED
